68a0733faac2057e7e27ccbbfc861ab4217b89e8
EFSESU <145917782+EFSESU@users.noreply.github.com>
2024-10-14 14:20:33 +0800

    1

---
+++ A3/nodejs/routes/addadmin.js
@@ -0,0 +1,11 @@
+const express = require('express') //创建路由实例
+const router = express.Router()
+
+router.get('/',(req,res) => {
+
+  res.render('addadmin.html')
+})
+
+
+
+module.exports = router  //暴露接口
